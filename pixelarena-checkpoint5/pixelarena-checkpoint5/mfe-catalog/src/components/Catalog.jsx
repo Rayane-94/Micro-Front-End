c1968ac662d63e41540b124dd1ddad4c28867607
Rayane Besrour <rayanebesrour3@gmail.com>
2026-03-03 14:29:29 +0100

    Checkpoint 5

--- pixelarena-checkpoint5/pixelarena-checkpoint5/mfe-catalog/src/components/Catalog.jsx
+++ pixelarena-checkpoint5/pixelarena-checkpoint5/mfe-catalog/src/components/Catalog.jsx
@@ -13,8 +13,8 @@ const PRODUCTS = [
 
 function ProductCard({ product }) {
   const handleAddToCart = () => {
-    // TODO: notifie l'eventBus que ce produit a été ajouté au panier
-    // L'événement doit transmettre : id, name, price
+    eventBus.emit('cart:add', { id: product.id, name: product.name, price: product.price });
+    console.log('[EventBus] cart:add', { id: product.id, name: product.name, price: product.price });
   };
 
   return (
